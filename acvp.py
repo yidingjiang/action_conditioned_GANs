1346aefbf6b89103bdaa90321ae8cca4758fd2a7
YiDing Jiang <yidinggjiangg@gmail.com>
2017-04-23 10:37:54 -0700

    implemented 3 streams architecture

--- acvp.py
+++ acvp.py
@@ -57,7 +57,6 @@ def save_samples(output_path, input_sample, generated_sample, gt, sample_number)
             frame = vid[:,:,3*j:3*(j+1)]
             plt.imsave(save_path, frame[:,:,::-1])
 
-
 def build_generator(images, actions, reuse=False):
     with tf.variable_scope('g', reuse=reuse):
         out = slim.conv2d(
@@ -141,6 +140,103 @@ def build_generator(images, actions, reuse=False):
             padding='SAME',
             reuse=reuse)
     return out
+
+def decoder_block(input,
+                    name,
+                    output_fn=tf.tanh,
+                    reuse=False):
+    out = slim.conv2d_transpose(
+        input,
+        256,
+        [5, 5],
+        activation_fn=tf.nn.relu,
+        stride=2,
+        scope= name + 'tconv1',
+        padding='SAME',
+        normalizer_fn=slim.batch_norm,
+        reuse=reuse)
+    out = slim.conv2d_transpose(
+        out,
+        128,
+        [5, 5],
+        activation_fn=tf.nn.relu,
+        stride=2,
+        scope= name + 'tconv2',
+        padding='SAME',
+        normalizer_fn=slim.batch_norm,
+        reuse=reuse)
+    out = slim.conv2d_transpose(
+        out,
+        64,
+        [5, 5],
+        activation_fn=tf.nn.relu,
+        stride=2,
+        scope= name + 'tconv3',
+        padding='SAME',
+        normalizer_fn=slim.batch_norm,
+        reuse=reuse)
+    out = slim.conv2d_transpose(
+        out,
+        3,
+        [5, 5],
+        activation_fn=output_fn,
+        stride=2,
+        scope= name + 'tconv4',
+        padding='SAME',
+        reuse=reuse)
+    return out
+
+def build_generator_atn(inputs,
+                        actions,
+                        reuse=False):
+    with tf.variable_scope('g', reuse=reuse):
+        out = slim.conv2d(
+            inputs,
+            64,
+            [5, 5],
+            activation_fn=tf.nn.relu,
+            stride=2,
+            scope='conv1',
+            padding='SAME',
+            normalizer_fn=slim.batch_norm,
+            reuse=reuse)
+        out = slim.conv2d(
+            out,
+            128,
+            [5, 5],
+            activation_fn=tf.nn.relu,
+            stride=2,
+            scope='conv2',
+            padding='SAME',
+            normalizer_fn=slim.batch_norm,
+            reuse=reuse)
+        out = slim.conv2d(
+            out,
+            256,
+            [5, 5],
+            activation_fn=tf.nn.relu,
+            stride=2,
+            scope='conv3',
+            padding='SAME',
+            normalizer_fn=slim.batch_norm,
+            reuse=reuse)
+        out = slim.conv2d(
+            out,
+            512,
+            [5, 5],
+            activation_fn=tf.nn.relu,
+            stride=2,
+            scope='conv4',
+            padding='SAME',
+            normalizer_fn=slim.batch_norm,
+            reuse=reuse)
+        out = tf.concat(values=[out, actions], axis=3)
+
+        foreground = decoder_block(out, name='foreground')
+        mask = decoder_block(out, name='mask', output_fn=tf.nn.sigmoid)
+        background = decoder_block(out, name='background')
+
+        return foreground, mask, background
 
 def build_discriminator(inputs,
                         actions,
@@ -251,7 +347,7 @@ def build_psnr(true, pred):
 
 
 class Trainer():
-    def __init__(self, sess, arg_adv, arg_loss, arg_opt, arg_residual):
+    def __init__(self, sess, arg_adv, arg_loss, arg_opt, arg_residual, arg_attention):
         self.sess = sess
 
         self.img_ph = tf.placeholder(
@@ -270,13 +366,21 @@ class Trainer():
         reshaped_actions = tf.reshape(self.action_ph, [tf.shape(self.action_ph)[0], 1, 1, 10])
         reshaped_actions = tf.tile(reshaped_actions, [1, 4, 4, 1])
         reshaped_actions_d = tf.tile(reshaped_actions, [1, 4, 4, 1])
-        self.g_out = build_generator(self.img_ph, reshaped_actions)
-        if arg_residual:
-            self.g_next_frame = self.img_ph + self.g_out
-            gt_output = self.next_frame_ph - self.img_ph
-        else:
-            self.g_next_frame = self.g_out
+
+        if arg_attention:
             gt_output = self.next_frame_ph
+            self.foreground, self.mask, self.background = build_generator_atn(self.img_ph, reshaped_actions)
+            negative_mask = tf.ones_like(self.mask) - self.mask
+            self.g_out = tf.multiply(self.mask, self.foreground) + tf.multiply(negative_mask, self.background)
+            self.g_next_frame = self.g_out
+        else:
+            self.g_out = build_generator(self.img_ph, reshaped_actions)
+            if arg_residual:
+                self.g_next_frame = self.img_ph + self.g_out
+                gt_output = self.next_frame_ph - self.img_ph
+            else:
+                self.g_next_frame = self.g_out
+                gt_output = self.next_frame_ph
 
         self.d_out_gen = build_discriminator(
             tf.concat(values=[self.img_ph, self.g_next_frame], axis=3),
@@ -292,11 +396,11 @@ class Trainer():
         #     self.g_out, gt_output)
         g_l2_loss = tf.norm(self.g_out - gt_output, ord=1, axis=None, keep_dims=False, name='l1_difference')
         if arg_residual:
-            g_l2_loss *= 10
+            g_l2_loss *= 100
 
         if arg_adv:
             g_adv_loss = build_g_adv_loss(self.d_out_gen, arg_loss)
-            self.g_loss = 0.1*g_l2_loss + g_adv_loss
+            self.g_loss = 0.2*g_l2_loss + g_adv_loss
         else:
             self.g_loss = g_l2_loss
 
@@ -371,7 +475,7 @@ class Trainer():
         return gen_next_frames, summ
 
 
-def train(input_path, output_path, test_output_path, log_dir, model_dir, arg_adv, arg_loss, arg_opt, arg_residual):
+def train(input_path, output_path, test_output_path, log_dir, model_dir, arg_adv, arg_loss, arg_opt, arg_residual, arg_attention):
     img_data_train, action_data_train = load_tfrecord.build_tfrecord_input(
         BATCH_SIZE,
         input_path,
@@ -384,7 +488,7 @@ def train(input_path, output_path, test_output_path, log_dir, model_dir, arg_adv
     action_data_test = tf.squeeze(action_data_test[:,0,:])
     with tf.Session() as sess:
         tf.train.start_queue_runners(sess)
-        trainer = Trainer(sess, arg_adv, arg_loss, arg_opt, arg_residual)
+        trainer = Trainer(sess, arg_adv, arg_loss, arg_opt, arg_residual, arg_attention)
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         writer = tf.summary.FileWriter(
@@ -438,6 +542,7 @@ if __name__ == '__main__':
     parser.add_argument('--loss', type=str, default='wass')
     parser.add_argument('--opt', type=str, default='rmsprop')
     parser.add_argument('--residual', action='store_true')
+    parser.add_argument('--attention', action='store_true')
     args = parser.parse_args()
     output_path = os.path.join(args.output_path, 'train_output')
     test_output_path = os.path.join(args.output_path, 'test_output')
@@ -453,4 +558,5 @@ if __name__ == '__main__':
           args.adv,
           args.loss,
           args.opt,
-          args.residual)
+          args.residual,
+          args.attention)
