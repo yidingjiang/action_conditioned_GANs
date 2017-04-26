ac33370b7929bc4a0a46b8c84940993bbdc855ab
Allan Zhou <allan.zhou@berkeley.edu>
2017-04-26 02:20:55 -0700

    More batchnorm

--- spatiotemporal_model.py
+++ spatiotemporal_model.py
@@ -8,50 +8,31 @@ from keras_contrib.layers.convolutional import Deconvolution3D
 def build_generator(img_input):
     #img_input = tf.placeholder(tf.float32, [None, 2, 64, 64, 3])
     batch_size = tf.shape(img_input)[0]
+    conv_specs = [
+        (32, [1, 3, 3]),
+        (64, [1, 3, 3]),
+        (128, [1, 3, 3]),
+        (256, [1, 3, 3]),
+        (32, [2, 1, 1])
+    ]
     with tf.variable_scope('g'):
-        out = Conv3D(
-            32,
-            [1, 3, 3],
-            activation='relu',
-            padding='same',
-            name='conv1')(img_input)
-        out = BatchNormalization()(out)
-        out = Conv3D(
-            64,
-            [1, 3, 3],
-            activation='relu',
-            padding='same',
-            dilation_rate=[1, 2, 2],
-            name='conv2')(out)
-        out = BatchNormalization()(out)
-        out = Conv3D(
-            128,
-            [1, 3, 3],
-            activation='relu',
-            padding='same',
-            dilation_rate=[1, 4, 4],
-            name='conv3')(out)
-        out = BatchNormalization()(out)
-        out = Conv3D(
-            256,
-            [1, 3, 3],
-            activation='relu',
-            padding='same',
-            dilation_rate=[1, 8, 8],
-            name='conv4')(out)
-        out = BatchNormalization()(out)
-        out = Conv3D(
-            32,
-            [2, 1, 1],
-            name='conv5')(out)
-        out = BatchNormalization()(out)
+        out = img_input
+        bn_updates = []
+        for i, spec in conv_specs:
+            out = Conv3D(
+                spec[0],
+                spec[1],
+                activation='relu',
+                padding='same',
+                name='conv{:d}'.format(i))(out)
+            out = tf.layers.batch_normalization(out, training=True)
         out = Deconvolution3D(
             32,
             [2, 1, 1],
             activation='relu',
             output_shape=[None, 2, 64, 64, 32],
             name='tconv1')(out)
-        out = BatchNormalization()(out)
+        out = tf.layers.batch_normalization(out, training=True)
         out = Deconvolution3D(
             32,
             [2, 1, 1],
@@ -60,7 +41,7 @@ def build_generator(img_input):
             padding='same',
             output_shape=[None, 4, 64, 64, 32],
             name='tconv2')(out)
-        out = BatchNormalization()(out)
+        out = tf.layers.batch_normalization(out, training=True)
         out = Deconvolution3D(
             25,
             [2, 1, 1],
@@ -87,5 +68,6 @@ def build_generator(img_input):
             output_frames.append(frame_i)
 
         final_output = tf.stack(output_frames, axis=1)
-
+        # batchnorm update ops
+        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
         return final_output
