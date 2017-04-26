15a29aef5ae3c404637dfa201f90c4f0ad4b24a7
YiDing Jiang <yidinggjiangg@gmail.com>
2017-04-25 18:15:06 -0700

    correctly compiled aka dimensions working transformer

--- utils.py
+++ utils.py
@@ -168,11 +168,21 @@ def build_generator_transform(images, actions, batch_size, reuse=False, color_ch
             reuse=reuse)
         out = slim.conv2d_transpose(
             out,
+            128,
+            [5, 5],
+            activation_fn=tf.nn.relu,
+            stride=2,
+            scope='tconv3',
+            padding='SAME',
+            normalizer_fn=slim.batch_norm,
+            reuse=reuse)
+        out = slim.conv2d_transpose(
+            out,
             ksize*ksize,
             [5, 5],
             activation_fn=None,
             stride=2,
-            scope='tconv3',
+            scope='tconv4',
             padding='SAME',
             reuse=reuse)
 
@@ -182,6 +192,10 @@ def build_generator_transform(images, actions, batch_size, reuse=False, color_ch
                                                     strides=[1, 1, 1, 1],
                                                     rates=[1, 1, 1, 1],
                                                     padding='SAME')
+        print(input_extracted.get_shape())
+        input_extracted = tf.reshape(input_extracted, 
+                                        [batch_size, 64, 64, ksize*ksize, 3])
+        out = tf.stack([out]*3, axis=4)
         out *= input_extracted
         out = tf.reduce_mean(out, 3)
 
