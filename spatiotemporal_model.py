fe51a0f390c7275fd996af505d24b9b844c69e78
Allan Zhou <allan.zhou@berkeley.edu>
2017-04-27 19:07:35 -0700

    Action conditioning

--- spatiotemporal_model.py
+++ spatiotemporal_model.py
@@ -9,18 +9,29 @@ from utils import lrelu, build_tfrecord_input, save_samples
 
 
 class Model():
-    def __init__(self, sess, sequence, arg_g_loss):
+    def __init__(self, sess, sequence, actions, arg_g_loss, arg_actions):
         self.sess = sess
         self.sequence = sequence
         input_images = self.sequence[:,:2,:,:,:]
+        tiled_actions = None
+        d_actions = None
+        if arg_actions:
+            actions_by_ts = []
+            for i in range(1, 9):
+                actions_by_ts.append(actions[:,i,:])
+            reshaped_actions = tf.concat(
+                axis=1, values=actions_by_ts)[:,None,None, None,:]
+            tiled_actions = tf.tile(
+                reshaped_actions, [1, 1, 64, 64, 1])
+            d_actions = tf.tile(actions[:,:,None,None,:], [1, 1, 64, 64, 1])
 
         with tf.variable_scope('g'):
-            self.g_out = self._build_generator(input_images)
+            self.g_out = self._build_generator(input_images, tiled_actions)
         with tf.variable_scope('d'):
             d_conv_layers = self._build_d_conv_layers()
-            self.d_real = self._build_discriminator(self.sequence, d_conv_layers)
+            self.d_real = self._build_discriminator(self.sequence, d_conv_layers, d_actions)
         with tf.variable_scope('d', reuse=True):
-            self.d_gen = self._build_discriminator(self.g_out, d_conv_layers)
+            self.d_gen = self._build_discriminator(self.g_out, d_conv_layers, d_actions)
 
         self.g_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'g')
         self.d_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'd')
@@ -33,6 +44,7 @@ class Model():
         l_ord = 2
         g_lp_loss = tf.reduce_mean(tf.reduce_sum(
             tf.abs(self.g_out - self.sequence)**l_ord, axis=[1, 2, 3, 4]))
+
         g_adv_loss = tf.losses.sigmoid_cross_entropy(
             tf.ones_like(self.d_gen), self.d_gen)
         if arg_g_loss == 'l2':
@@ -40,7 +52,7 @@ class Model():
         elif arg_g_loss == 'adv':
             self.g_loss = g_adv_loss
         else:
-            self.g_loss = g_lp_loss + g_adv_loss
+            self.g_loss = .005 * g_lp_loss + g_adv_loss
         d_real_loss = tf.losses.sigmoid_cross_entropy(
             0.9 * tf.ones_like(self.d_real), self.d_real)
         d_gen_loss = tf.losses.sigmoid_cross_entropy(
@@ -103,8 +115,10 @@ class Model():
         return d_conv_layers
 
 
-    def _build_discriminator(self, output, d_conv_layers):
+    def _build_discriminator(self, output, d_conv_layers, d_actions):
         out = output
+        if d_actions is not None:
+            out = tf.concat(values=[out, d_actions], axis=-1)
         for i, layer in enumerate(d_conv_layers):
             out = layer(out)
             if i < len(d_conv_layers) - 1:
@@ -116,7 +130,7 @@ class Model():
         return out
 
 
-    def _build_generator(self, img_input):
+    def _build_generator(self, img_input, tiled_actions):
         ksize = 10
         batch_size = tf.shape(img_input)[0]
         conv_specs = [
@@ -138,6 +152,8 @@ class Model():
                 out,
                 training=True,
                 name='bn{:d}'.format(i))
+        if tiled_actions is not None:
+            out = tf.concat(values=[out, tiled_actions], axis=-1)
         out = Deconvolution3D(
             32,
             [2, 1, 1],
@@ -199,6 +215,7 @@ if __name__ == '__main__':
     parser.add_argument('--g_loss', type=str, default='l2')
     parser.add_argument('--batch_size', type=int, default=4)
     parser.add_argument('--iterations', type=int, default=10000)
+    parser.add_argument('--actions', action='store_true')
     args = parser.parse_args()
     train_output_path = os.path.join(args.output_path, 'train_output')
     model_dir = os.path.join(args.output_path, 'models')
@@ -211,7 +228,7 @@ if __name__ == '__main__':
         10, .95, True)
     with tf.Session() as sess:
         tf.train.start_queue_runners(sess)
-        m = Model(sess, sequence, args.g_loss)
+        m = Model(sess, sequence, actions, args.g_loss, args.actions)
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
         train_writer = tf.summary.FileWriter(
